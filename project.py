94ac97d0acfdc732cf261ca878c647bb51f4c9b4
Sajeel-Nadeem <sajeel.nadeem05@gmail.com>
2025-11-29 15:37:24 +0500

    Sajeel's Work

--- project.py
+++ project.py
@@ -23,3 +23,32 @@ def Muhammad_Ali():
 
 
 Muhammad_Ali()
+
+import random
+
+def Sajeel_Nadeem():
+    play_again = "yes"
+
+    for _ in range(100):  # Just a large loop so game can restart
+        if play_again.lower() != "yes":
+            break
+
+        secret = random.randint(1, 50)
+        attempts = 0
+        print("\nThink of a number between 1 and 50:\n")
+
+        for _ in range(100):  # Loop for guessing
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess > secret:
+                print("Too high!")    # This will indicate that the number is higher
+            elif guess < secret:
+                print("Too low!")     # This will indicate that the number is lower
+            else:
+                print(f"Correct! You guessed it in {attempts} attempts.")
+                break
+
+        play_again = input("\nDo you want to play again? (yes/no): ")
+
+Sajeel_Nadeem()
